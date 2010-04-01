12bdb00ff41dcc4b2146294420ff036ffbbb9d12
zhoushuyan <zhoushuyan@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-01 06:18:29 +0000

    初始化Demo工程

---
+++ trunk/src/ZooDemo/Classes/ZooDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ZooDemoAppDelegate.h
+//  ZooDemo
+//
+//  Created by Zhou Shuyan on 10-4-1.
+//  Copyright Apple Inc 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZooDemoAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
