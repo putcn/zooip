eb9bfc50058431fb1d53e5c216e550cc9fff8028
jamerainbow <jamerainbow@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-01 14:56:56 +0000

    增加蛋,蛇以及食盆到Demo的背景中

--- src/ZooDemo/Classes/HelloWorldScene.m
+++ src/ZooDemo/Classes/HelloWorldScene.m
@@ -10,6 +10,19 @@ CCSprite *background;
 CCSprite *tree;
 CCSprite *house;
 CCSprite *waterbox;
+CCSprite *egg1;
+CCSprite *egg2;
+CCSprite *egg3;
+CCSprite *egg4;
+CCSprite *egg5;
+CCSprite *egg6;
+CCSprite *egg7;
+CCSprite *egg8;
+CCSprite *egg9;
+CCSprite *egg10;
+CCSprite *pan;
+CCSprite *snake1;
+CCSprite *snake2;
 
 // HelloWorld implementation
 @implementation HelloWorld
@@ -38,20 +51,73 @@ CCSprite *waterbox;
 		
 		//set the backgroud and the size set 50%
 		background = [CCSprite spriteWithFile:@"bgimg.jpg"];
-		background.scale = 0.5f;
+		background.scale = 0.47f;
 		background.position = ccp(240,160);
 		
 		tree = [CCSprite spriteWithFile:@"tree.png"];
-		tree.position = ccp(800,450);
+		tree.position = ccp(810,440);
 		[background addChild:tree];
 		
 		house = [CCSprite spriteWithFile:@"house.png"];
-		house.position = ccp(470,480);
+		house.position = ccp(480,480);
 		[background addChild:house];
 		
 		waterbox = [CCSprite spriteWithFile:@"waterbox.png"];
-		waterbox.position = ccp(360,480);
+		waterbox.position = ccp(350,480);
 		[background addChild:waterbox];
+		
+		egg1 = [CCSprite spriteWithFile:@"丹顶鹤蛋.png"];
+		egg1.position = ccp(600,100);
+		[background addChild:egg1 z:1];
+		
+		egg2 = [CCSprite spriteWithFile:@"喜鹊蛋.png"];
+		egg2.position = ccp(710,180);
+		[background addChild:egg2 z:1];
+		
+		egg3 = [CCSprite spriteWithFile:@"大雁蛋.png"];
+		egg3.position = ccp(820,146);
+		[background addChild:egg3 z:1];
+		
+		egg4 = [CCSprite spriteWithFile:@"天鹅蛋.png"];
+		egg4.position = ccp(630,190);
+		[background addChild:egg4 z:1];
+		
+		egg5 = [CCSprite spriteWithFile:@"孔雀蛋.png"];
+		egg5.position = ccp(580,134);
+		[background addChild:egg5 z:1];
+		
+		egg6 = [CCSprite spriteWithFile:@"火鸡蛋.png"];
+		egg6.position = ccp(750,167);
+		[background addChild:egg6 z:1];
+		
+		egg7 = [CCSprite spriteWithFile:@"野鸡蛋.png"];
+		egg7.position = ccp(660,123);
+		[background addChild:egg7 z:1];
+		
+		egg8 = [CCSprite spriteWithFile:@"野鸭蛋.png"];
+		egg8.position = ccp(570,104);
+		[background addChild:egg8 z:1];
+		
+		egg9 = [CCSprite spriteWithFile:@"鸡蛋.png"];
+		egg9.position = ccp(780,200);
+		[background addChild:egg9 z:1];
+		
+		egg10 = [CCSprite spriteWithFile:@"鸳鸯蛋.png"];
+		egg10.position = ccp(790,178);
+		[background addChild:egg10 z:1];
+		
+		pan = [CCSprite spriteWithFile:@"喂食3.png"];
+		pan.position = ccp(300,150);
+		[background addChild:pan z:1];
+		
+		snake1 = [CCSprite spriteWithFile:@"snake_sleep_2.png"];
+		snake1.position = ccp(800,100);
+		[background addChild:snake1 z:2];
+		
+		snake2 = [CCSprite spriteWithFile:@"snake_sleep_5.png"];
+		snake2.position = ccp(600,200);
+		[background addChild:snake2 z:2];
+		
 		
 		[self addChild: background z:0];
 		
