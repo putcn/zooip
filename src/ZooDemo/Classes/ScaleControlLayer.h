21b202bafd5e4deccc2a2a675b558502fe0694f5
njx.2008 <njx.2008@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-07 06:27:00 +0000

    Add scale control logic

---
+++ src/ZooDemo/Classes/ScaleControlLayer.h
@@ -0,0 +1,17 @@
+//
+//  ScaleControlLayer.h
+//  ZooDemo
+//
+//  Created by Niu Darcy on 4/7/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+
+@interface ScaleControlLayer : CCLayer
+{
+	
+}
+
+@end
---
+++ src/ZooDemo/Classes/ScaleControlLayer.m
@@ -0,0 +1,14 @@
+//
+//  ScaleControlLayer.m
+//  ZooDemo
+//
+//  Created by Niu Darcy on 4/7/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "ScaleControlLayer.h"
+
+
+@implementation ScaleControlLayer
+
+@end
