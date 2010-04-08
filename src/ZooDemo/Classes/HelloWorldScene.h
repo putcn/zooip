bc37364276ddbc26ef7f0b011be447f4e71e558a
njx.2008 <njx.2008@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-08 14:42:36 +0000

    add drag and limit logic

--- src/ZooDemo/Classes/HelloWorldScene.h
+++ src/ZooDemo/Classes/HelloWorldScene.h
@@ -5,7 +5,24 @@
 // HelloWorld Layer
 @interface HelloWorld : CCLayer
 {
-
+	CCSprite *baseContainer;
+	CCSprite *background;
+	CCSprite *tree;
+	CCSprite *house;
+	CCSprite *waterbox;
+	CCSprite *egg1;
+	CCSprite *egg2;
+	CCSprite *egg3;
+	CCSprite *egg4;
+	CCSprite *egg5;
+	CCSprite *egg6;
+	CCSprite *egg7;
+	CCSprite *egg8;
+	CCSprite *egg9;
+	CCSprite *egg10;
+	CCSprite *pan;
+	CCSprite *snake1;
+	CCSprite *snake2;
 }
 
 // returns a Scene that contains the HelloWorld as the only child
