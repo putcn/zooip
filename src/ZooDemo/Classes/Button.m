466cd6bb053d7fafa450c72141148dd6d2be35a8
happygulei5265 <happygulei5265@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-08 10:48:46 +0000

    Add the UI logic

---
+++ src/ZooDemo/Classes/Button.m
@@ -0,0 +1,97 @@
+//
+//  Button.m
+//  
+//
+//  Created by Niu Darcy on 2/3/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Button.h"
+
+
+@implementation Button
+
+@synthesize label;
+
+-(id) initWithLabel:(NSString*) labelText setColor:(ccColor3B) labelColor setFont:(NSString*) labelFont
+		setSize:(int) labelSize setBackground:(NSString*) imagePath setTarget:(id) target setSelector:(SEL) handler 
+		setPriority:(int) priorityValue offsetX:(int) offsetXValue offsetY:(int) offsetYValue
+{
+	if( (self=[super init] ))
+	{
+		CCTexture2D *bg = [ [CCTexture2D alloc] initWithImage: [UIImage imageWithContentsOfFile: [[NSBundle mainBundle] pathForResource:imagePath ofType:nil] ] ];
+		CGRect rect = CGRectZero;
+		rect.size = bg.contentSize;
+		[self setTexture: bg];
+		[self setTextureRect: rect];
+		[bg release];
+		
+		CGSize s = [self.texture contentSize];
+		
+		text = [CCLabel labelWithString:labelText fontName:labelFont fontSize:labelSize];
+		text.position = ccp( s.width / 2 + offsetXValue , s.height / 2 + offsetYValue );
+		text.color = labelColor;
+		[self addChild:text];
+		
+		targetCallBack = [target retain];
+		selector = handler;
+		
+		pri = priorityValue;
+	}
+	self.scale = 0.75f;
+	return self;
+}
+
+-(void) dealloc
+{
+	[label release];
+	[text release];
+	[targetCallBack release];
+	[super dealloc];
+}
+
+-(void) setLabel:(NSString*) labelValue
+{
+	[text setString: labelValue];
+}
+
+- (CGRect)rect
+{
+	CGSize s = [self.texture contentSize];
+	return CGRectMake(-s.width/2, -s.height/2, s.width, s.height);
+}
+
+- (BOOL)containsTouchLocation:(UITouch *)touch
+{
+	return CGRectContainsPoint(self.rect, [self convertTouchToNodeSpaceAR:touch]);
+}
+
+- (void)onEnter
+{
+	[[CCTouchDispatcher sharedDispatcher] addTargetedDelegate:self priority:pri swallowsTouches:YES];
+	[super onEnter];
+}
+
+- (void)onExit
+{
+	[[CCTouchDispatcher sharedDispatcher] removeDelegate:self];
+	[super onExit];
+}	
+
+- (BOOL)ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	if ( ![self containsTouchLocation:touch] || !self.visible ) return NO;
+	self.scale = 1;
+	return YES;
+}
+
+-(void) ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	self.scale = 0.75f;
+	if (selector != nil && [self containsTouchLocation:touch])
+	{
+		[targetCallBack performSelector:selector withObject:self];
+	}
+}
+
+@end
---
+++ src/ZooDemo/Classes/ZooManager.h
@@ -0,0 +1,20 @@
+//
+//  ZooManager.h
+//  ZooDemo
+//
+//  Created by Gu Lei on 10-4-7.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "Button.h"
+
+@interface ZooManager : CCSprite
+{
+	
+}
+
+-(void) addButton;
+
+@end
