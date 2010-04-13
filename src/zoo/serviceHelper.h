70204ef03021c2bc4189fb45a7190e92e2349c21
Darcy <putcncx@gmail.com>
2010-04-13 07:25:25 +0000

    service getFarmInfo,getAllBirdFarmAnimalInfo added

--- src/zoo/serviceHelper.h
+++ src/zoo/serviceHelper.h
@@ -6,6 +6,11 @@
 //  Copyright 2010 Apple Inc. All rights reserved.
 //
 
+// testing farmer id : A6215BF61A3AF50A8F72F043A1A6A85C
+// testing uid : 25313321
+// testing pid : 11
+// 
+
 #import <Foundation/Foundation.h>
 #import "ASIFormDataRequest.h"
 
@@ -17,4 +22,6 @@
 - (void) restore;
 -(ASIFormDataRequest *)BuildRequestWithURL:(NSString *)URLString AndRequestFlag:(NSString *)requestFlag AndCallBackScope:(id)CallBackDelegate AndSuccessSel:(NSString *)SuccessSelector AndFailedSel:(NSString *)FailedSelector;
 -(void)connectivityTestWithScope:(id)CallBackDelegate AndSuccessSel:(NSString *)SuccessSelector AndFailedSel:(NSString *)FailedSelector;
+-(void)getFarmInfoWithFarmerId:(NSString *)farmerId AndIsbodyGarded:(BOOL)IsbodyGarded AndScope:(id)CallBackDelegate AndSuccessSel:(NSString *)SuccessSelector AndFailedSel:(NSString *)FailedSelector;
+-(void)getAllBirdFarmAnimalInfoWithFarmId:(NSString *)farmerId AndFarmerId:(NSString *)farmerId AndScope:(id)CallBackDelegate AndSuccessSel:(NSString *)SuccessSelector AndFailedSel:(NSString *)FailedSelector;
 @end
