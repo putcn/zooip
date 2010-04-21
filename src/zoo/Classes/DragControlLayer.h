f6684bee6943e2163ed943ce79e4df5c8f8c3ef6
happygulei5265 <happygulei5265@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-21 02:11:02 +0000

    Add the Scale/Drag UI Control

---
+++ src/zoo/Classes/DragControlLayer.h
@@ -0,0 +1,25 @@
+//
+//  DragControlLayer.h
+//  Zoo
+//
+//  Created by Niu Darcy on 4/21/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+
+@interface DragControlLayer : CCLayer
+{
+	CGFloat oX;
+	CGFloat oY;
+	
+	CCSprite *target;
+	
+	BOOL isReset;
+	NSInteger curTouchesNum;
+}
+
+-(id)initWithTarget:(CCSprite*) targetValue;
+
+@end
