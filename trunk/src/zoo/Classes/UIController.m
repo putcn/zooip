2f524438fd9eb539766bd085299b822621bb7c28
happygulei5265 <happygulei5265@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-27 07:29:47 +0000

    Add the UI resource

--- trunk/src/zoo/Classes/UIController.m
+++ trunk/src/zoo/Classes/UIController.m
@@ -11,4 +11,35 @@
 
 @implementation UIController
 
+static UIController *_sharedUIController = nil;
+
++(UIController *)sharedUIController
+{
+	@synchronized([UIController class])
+	{
+		if (!_sharedUIController)
+		{
+			_sharedUIController = [[UIController alloc] init];
+		}
+		
+		return _sharedUIController;
+	}
+	
+	return nil;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	NSLog(@"Value %@ changed in %@", keyPath, [object description]);
+	if ([keyPath isEqual:@""])
+	{
+		NSMutableArray *array = [change objectForKey:NSKeyValueChangeNewKey];
+		
+		//		for(NSNumber *number in array)
+		//		{
+		//			NSLog(@"%i", [number integerValue]);
+		//		}
+	}
+}
+
 @end
