77262f8f7635f9aa32d482303b65a71a067a3c72
putcncx <putcncx@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-04-27 06:40:17 +0000

    data model for animal added

---
+++ trunk/src/zoo/Classes/DataModelAnimal.m
@@ -0,0 +1,80 @@
+//
+//  DataModelAnimal.m
+//  zoo
+//
+//  Created by Darcy on 4/26/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "DataModelAnimal.h"
+
+
+@implementation DataModelAnimal
+
+@synthesize adultStage;
+@synthesize aliveEdge;
+@synthesize animalName;
+@synthesize animalType;
+@synthesize antsPrice;
+@synthesize babyStage;
+@synthesize baseCycle;
+@synthesize baseInterval;
+@synthesize basePrice;
+@synthesize baseYield;
+@synthesize birdStage;
+@synthesize birdType;
+@synthesize characteristic;
+@synthesize coupleId;
+@synthesize currentInterval;
+@synthesize discount;
+@synthesize eggId;
+@synthesize eggImgId;
+@synthesize eggPrice;
+@synthesize emotion;
+@synthesize experience;
+@synthesize farmType;
+@synthesize flyingSpeed;
+@synthesize flyingStep;
+@synthesize gender;
+@synthesize hatchTime;
+@synthesize health;
+@synthesize lastOutputTime;
+@synthesize level;
+@synthesize levelRequired;
+@synthesize longevity;
+@synthesize originalAnimalId;
+@synthesize productId;
+@synthesize productionFlag;
+@synthesize runingSpeed;
+@synthesize runingStep;
+@synthesize speed;
+@synthesize stageEndTime;
+@synthesize status;
+@synthesize step;
+@synthesize swimmingSpeed;
+@synthesize swimmingStep;
+@synthesize walkToEatSpeed;
+@synthesize walkToEatStep;
+@synthesize yield;
+@synthesize youthStage;
+@synthesize zygotePrice;
+
+
+@synthesize animalId;
+@synthesize birthday;
+@synthesize coupleAnimalId;
+@synthesize description;
+@synthesize eggDescription;
+@synthesize eggName;
+@synthesize eggNameEN;
+@synthesize farmId;
+@synthesize lastFeedTime;
+@synthesize marriageDate;
+@synthesize picturePrefix;
+@synthesize scientificNameCN;
+@synthesize scientificNameEN;
+@synthesize sickStartTime;
+@synthesize speedFlag;
+@synthesize virusReleaserId;
+
+@end
---
+++ trunk/src/zoo/Classes/DataModelCommon.h
@@ -0,0 +1,16 @@
+//
+//  DataModelCommon.h
+//  zoo
+//
+//  Created by Darcy on 4/26/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DataModelCommon : NSObject {
+	
+}
+
+@end
