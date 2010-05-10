b5608f33acd4dcbc8a4b739def4d79147fb495ed
jamerainbow <jamerainbow@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-05-10 08:06:46 +0000

    add dejecta

---
+++ trunk/src/zoo/Classes/Dejecta.h
@@ -0,0 +1,18 @@
+//
+//  Dejecta.h
+//  zoo
+//
+//  Created by Rainbow on 5/10/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+CCTexture2D *dejecta;
+CGRect rect;
+
+@interface Dejecta : CCSprite {
+
+}
+
+@end
