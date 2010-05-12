88bf2dd70d7530e28b68859511303c3a61050211
jamerainbow <jamerainbow@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-05-12 05:34:46 +0000

    Add egg factory

---
+++ src/zoo/Classes/EggViewFactory.h
@@ -0,0 +1,31 @@
+//
+//  EggViewFactory.h
+//  zoo
+//
+//  Created by Rainbow on 5/12/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "EggView.h"
+#import "CraneEggView.h"
+#import "DuckEggView.h"
+#import "GooseEggView.h"
+#import "HenEggView.h"
+#import "MagpieEggView.h"
+#import "MallardEggView.h"
+#import "MandarinduckEggView.h"
+#import "ParrotEggView.h"
+#import "PeahenEggView.h"
+#import "PheasantEggView.h"
+#import "PigeonEggView.h"
+#import "SwanEggView.h"
+#import "TurkeyEggView.h"
+#import "WildgooseEggView.h"
+
+
+@interface EggViewFactory : NSObject {
+
+}
++(EggView *)createEggViews: (int)type;
+
+@end
