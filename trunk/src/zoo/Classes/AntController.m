3ea72ae20fa85e1b32e36e031f45698dde159b71
jamerainbow <jamerainbow@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-05-19 16:01:52 +0000

    implement eggs and bowls

---
+++ trunk/src/zoo/Classes/AntController.m
@@ -0,0 +1,14 @@
+//
+//  AntController.m
+//  zoo
+//
+//  Created by Rainbow on 5/19/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "AntController.h"
+
+
+@implementation AntController
+
+@end
