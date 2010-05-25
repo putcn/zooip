926131ff4f738621ebdedf6a3b218f6d2ff0e299
jamerainbow <jamerainbow@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-05-25 01:45:47 +0000

    Animal testing

--- src/zoo/Classes/HelloWorldScene.m
+++ src/zoo/Classes/HelloWorldScene.m
@@ -10,6 +10,7 @@
 #import "CraneEggView.h"
 #import "Animal.h"
 #import "UILayer.h"
+#import "ManageContainer.h"
 
 // HelloWorld implementation
 @implementation HelloWorld
@@ -47,7 +48,7 @@
 		baseContainer.position = ccp(-size.width / 2, -size.height / 2);
 		baseContainer.scale = 0.5f;
 		
-		CCSprite *background = [CCSprite spriteWithFile:@"bgimg.jpg"];
+		background = [CCSprite spriteWithFile:@"bgimg.jpg"];
 		background.scale = 0.95f;
 		background.position = ccp(480,320);
 		
@@ -56,43 +57,29 @@
 		CCSprite *tree = [CCSprite spriteWithFile:@"tree.png"];
 		tree.position = ccp(750,400);
 		
-		[baseContainer addChild:tree z:1];
+		[background addChild:tree z:1];
 		
 		CCSprite *house = [CCSprite spriteWithFile:@"house.png"];
 		house.position = ccp(450,410);
 		
-		[baseContainer addChild:house z:1];
+		[background addChild:house z:1];
 		
 		CCSprite *waterbox = [CCSprite spriteWithFile:@"waterbox.png"];
 		waterbox.position = ccp(330,420);
 		
-		[baseContainer addChild:waterbox z:1];
+		[background addChild:waterbox z:1];
 		
 		// add the label as a child to this Layer
 		[scaleContainer addChild: baseContainer];
 		
 		UILayer *uiLayer = [UILayer node];
 		[self addChild:uiLayer];
+
 		
-		
+		PigeonView * pigeonView = [[PigeonView alloc] init];
+		[[Animal alloc] initWithView:pigeonView setSpeed:0.5f setLimitRect:CGRectMake(100, 100, 200, 200)];
+		[background addChild:pigeonView];
 	}
-	
-	
-	//[[serviceHelper sharedService] getFarmInfoWithFarmerId:NULL AndIsbodyGarded:NO AndScope:self AndSuccessSel:@"requestDoneWith:" AndFailedSel:@"requestFailedWithReason:"];
-	//[[ServiceHelper sharedService] getFarmerInfo];
-	//[[ServiceHelper sharedService] getFarmInfoWithFarmerId:@"A6215BF61A3AF50A8F72F043A1A6A85C" AndIsGuarded:NO];
-	/*
-	[[ServiceHelper sharedService] getAllBirdFarmAnimalInfoWithFarmId:@"163D7A78682082B36872659C7A9DA8F9" AndFarmerId:@"A6215BF61A3AF50A8F72F043A1A6A85C"];
-	
-	 */
-	PigeonView *pigeonView = [[PigeonView alloc] init];
-	pigeonView.position = ccp(200,200);
-	[[Animal alloc] initWithView:pigeonView setSpeed:0.5f setLimitRect:CGRectMake(100, 100, 500, 500)];
-	[baseContainer addChild:pigeonView z:4];
-	
-	CraneEggView *craneEggView = [[CraneEggView alloc] init];
-	craneEggView.position = ccp(400,100);
-	[baseContainer addChild:craneEggView z:6];
 	return self;
 }
 
