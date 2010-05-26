eecdd3e51f1152896104382952b3fe68df86cfc1
njx.2008 <njx.2008@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-05-26 09:37:03 +0000

    add collision helper class

---
+++ src/zoo/Classes/CollisionHelper.m
@@ -0,0 +1,48 @@
+//
+//  CollisionHelper.m
+//  zoo
+//
+//  Created by Niu Darcy on 5/24/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "CollisionHelper.h"
+
+
+@implementation CollisionHelper
+
+static const UInt32 *collisionMap;
+static int width;
+static int height;
+
++(void)initCollisionMap
+{
+	UIImage *collisionImage = [UIImage imageNamed:@"collision_map.png"];
+	
+	width = (int)collisionImage.size.width;
+	height = (int)collisionImage.size.height;
+	
+	//unsigned char *collisionMap = (unsigned char *)malloc(width * height);
+	//memset( collisionMap, 0, width * height );
+	
+	CFDataRef imageData = CGDataProviderCopyData(CGImageGetDataProvider(collisionImage.CGImage));
+	collisionMap = (const UInt32*)CFDataGetBytePtr(imageData);
+}
+
++(int)getMapType:(CGPoint)point
+{
+	int x = (int)point.x;
+	int y = (int)point.y;
+	
+	
+	UInt32 pixel = collisionMap[(y*width)+x];
+	
+	if ((pixel & 0xff000000) == 0) return -1; // Limited ..
+	if (pixel & 0x00ff0000) return 0; // Land ..
+	if (pixel & 0x0000ff00) return 1; // Water ..
+	if (pixel & 0x000000ff) return 2; // Sky ..
+	
+	return -1;
+}
+
+@end
