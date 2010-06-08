7fbab53d37ce1ba7cb5d6b150a2f78892f020d73
alexliubo <alexliubo@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-06-08 01:27:11 +0000

    add the farm expansion panel

---
+++ src/zoo/Classes/AnimalExpansionPanel.h
@@ -0,0 +1,16 @@
+//
+//  AnimalExpansionPanel.h
+//  zoo
+//
+//  Created by AlexLiu on 6/8/10.
+//  Copyright 2010 Alex Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AnimalExpansionPanel : NSObject {
+
+}
+
+@end
