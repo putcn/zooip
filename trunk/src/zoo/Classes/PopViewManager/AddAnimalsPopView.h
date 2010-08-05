d7cbdd5299b075b58fe0561fac3e2131cea8ca65
alexliubo <alexliubo@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-08-05 02:46:46 +0000

    add the addAnimals to farm panel

---
+++ trunk/src/zoo/Classes/PopViewManager/AddAnimalsPopView.h
@@ -0,0 +1,18 @@
+//
+//  AddAnimalsPopView.h
+//  zoo
+//
+//  Created by AlexLiu on 8/5/10.
+//  Copyright 2010 Alex Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "popViewManager.h"
+#import "Common.h"
+
+
+@interface AddAnimalsPopView : NSObject {
+
+}
+
+@end
---
+++ trunk/src/zoo/Classes/PopViewManager/AddAnimalsPopView.m
@@ -0,0 +1,14 @@
+//
+//  AddAnimalsPopView.m
+//  zoo
+//
+//  Created by AlexLiu on 8/5/10.
+//  Copyright 2010 Alex Dev. All rights reserved.
+//
+
+#import "AddAnimalsPopView.h"
+
+
+@implementation AddAnimalsPopView
+
+@end
