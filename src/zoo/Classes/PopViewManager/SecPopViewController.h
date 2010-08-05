287eecf8296c724dd8869413500f93c6f559c21f
SLanxi <SLanxi@563bb6ae-dc5c-b7b5-8f5c-7262d9977941>
2010-08-05 09:25:17 +0000

    shop alpha

--- src/zoo/Classes/PopViewManager/SecPopViewController.h
+++ src/zoo/Classes/PopViewManager/SecPopViewController.h
@@ -13,20 +13,38 @@
 
 	int					m_npopViewType;
 	int					m_ntabFlag;
+	
+	int					buyAniId;
 	int					itemId;
+	int					tempPrice;
+	int					tempCount;
+	int					curr_itemType;
+	int					mixCount;
+	
 	NSString*			labelString;
-	IBOutlet UILabel*	myLabel;
-	IBOutlet UIImageView*showImage;
-	NSString*			curr_itemType;
+	NSString*			farmerId;
+	
+	IBOutlet UIImageView*	showImage;
+	IBOutlet UIImageView*	iconImage;
+	IBOutlet UILabel*		priceLabel;
+	IBOutlet UILabel*		nameLabel;
+	IBOutlet UILabel*		describeLabel;
+	IBOutlet UISlider*		buySlider;
+	IBOutlet UILabel*		countLabel;
+	IBOutlet UILabel*		wrongLabel;
 }
 
 @property(nonatomic, readwrite) int m_npopViewType;
 @property(nonatomic, readwrite) int m_ntabFlag;
 @property(nonatomic, readwrite) int itemId;
+@property(nonatomic, readwrite) int tempCount;
+@property(nonatomic, readwrite) int curr_itemType;
+
 @property(nonatomic, retain) NSString* labelString;
 
 - (IBAction) OKSelected:(id)sender;
 - (IBAction) CancelSelected:(id)sender;
+- (IBAction) sliderAction:(id)sender;
 
 - (void) setShowImageName:(NSString*)fileName;
 - (void) resultCallback:(NSObject *)value;
--- src/zoo/Classes/PopViewManager/SecPopViewController.m
+++ src/zoo/Classes/PopViewManager/SecPopViewController.m
@@ -20,7 +20,7 @@
 
 @implementation SecPopViewController
 
-@synthesize m_npopViewType, m_ntabFlag, itemId, labelString;
+@synthesize m_npopViewType, m_ntabFlag, itemId, curr_itemType, tempCount,labelString;
 
 
  // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
@@ -67,28 +67,75 @@
 	
 	[labelString release];
 	labelString = nil;
-	[myLabel release];
-	myLabel = nil;
+	[farmerId release];
+	farmerId = nil;
+	
+	[showImage release];
+	showImage = nil;
+	[iconImage release];
+	iconImage = nil;
+	[priceLabel release];
+	priceLabel = nil;
+	[nameLabel release];
+	nameLabel = nil;
+	[describeLabel release];
+	describeLabel = nil;
+	[buySlider release];
+	buySlider =nil;
+	[countLabel release];
+	countLabel = nil;
 	
     [super dealloc];
 }
 
+// slown down or speed up the slide show as the slider is moved
+- (IBAction)sliderAction:(id)sender
+{
+	UISlider* durationSlider = sender;
+//	self.imageView.animationDuration = [durationSlider value];
+//	if (!self.imageView.isAnimating)
+//		[self.imageView startAnimating];
+}
+
 - (IBAction) OKSelected:(id)sender{
 	
-	NSString *farmerId = [DataEnvironment sharedDataEnvironment].playerFarmerInfo.farmerId;
-//	NSMutableArray *animalIDs = (NSMutableArray *)[DataEnvironment sharedDataEnvironment].animalIDs;
-//	NSString* aniID = [animalIDs objectAtIndex:itemId];
-//	DataModelOriginalAnimal *serverAnimalData2 = (DataModelAnimal *)[[DataEnvironment sharedDataEnvironment].animals objectForKey:aniID];
-//	NSString* buyAniId = serverAnimalData2.originalAnimalId;
+	switch (m_ntabFlag) {
+		case BUY_ANIMAL:{
+			
+			//用蚂蚁购买
+			if (tempPrice > 0) {
+				
+				NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+										farmerId,									@"farmerId",
+										[NSString stringWithFormat:@"%d",buyAniId], @"originalAnimalId",
+										[NSString stringWithFormat:@"%d",tempCount],@"amount",
+										nil];
+				[[ServiceHelper sharedService] requestServerForMethod:ZooNetworkRequestbuyAnimalByAnts WithParameters:params AndCallBackScope:self AndSuccessSel:@"resultCallback:" AndFailedSel:@"faultCallback:"];
+			}
+			//用金币购买
+			else {
+				
+				NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+										farmerId,									@"farmerId",
+										[NSString stringWithFormat:@"%d",buyAniId], @"originalAnimalId",
+										[NSString stringWithFormat:@"%d",tempCount],@"amount",
+										nil];
+				[[ServiceHelper sharedService] requestServerForMethod:ZooNetworkRequestbuyAnimalByGoldenEgg WithParameters:params AndCallBackScope:self AndSuccessSel:@"resultCallback:" AndFailedSel:@"faultCallback:"];		
+			}
+		}
+			break;
+			
+		case SALE_ANIMAL:{
+			
+		}
+				break;
+
+		default:
+			break;
+	}
 	
-	NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
-							farmerId,							@"farmerId",
-							[NSString stringWithFormat:@"%d",itemId],@"originalAnimalId",
-							[NSString stringWithFormat:@"%d",1],@"amount",
-							nil];
-	[[ServiceHelper sharedService] requestServerForMethod:ZooNetworkRequestbuyAnimalByGoldenEgg WithParameters:params AndCallBackScope:self AndSuccessSel:@"resultCallback:" AndFailedSel:@"faultCallback:"];
-	
-	
+
+
 	[self.view removeFromSuperview];
 }
 
@@ -100,60 +147,100 @@
 - (void) setShowImageName:(NSString*)fileName{
 	
 	[showImage setImage:[UIImage imageNamed:fileName]];
+	
+	[DataEnvironment sharedDataEnvironment].playerFarmerInfo.antsCurrency;
+	
+	//get data
+	farmerId = [[NSString alloc] initWithString: [DataEnvironment sharedDataEnvironment].playerFarmerInfo.farmerId];
+	NSDictionary *originAnimalDic = (NSDictionary *)[DataEnvironment sharedDataEnvironment].originalAnimals;
+	DataModelOriginalAnimal *originAnimal; 
+	NSArray *animalArrayTemp = [originAnimalDic allKeys];
+	originAnimal = [originAnimalDic objectForKey:[animalArrayTemp objectAtIndex:itemId]];
+	
+	buyAniId = originAnimal.originalAnimalId;
+	tempCount = 1;
+	tempPrice = originAnimal.antsPrice;
+	
+	NSString* describeString = @"   性别：";
+	int income = 0;
+	
+	//show
+	if (tempPrice == 0) {
+		[iconImage setImage:[UIImage imageNamed:@"金蛋.png"]];
+		int basePrice = originAnimal.basePrice;
+		priceLabel.text = [NSString stringWithFormat:@"%d", basePrice];
+		describeString = [describeString stringByAppendingString:@"母\n   价格："];
+		describeString = [describeString stringByAppendingString:priceLabel.text];
+		describeString = [describeString stringByAppendingString:@"个金蛋"];
+		describeString = [describeString stringByAppendingString:@"\n总收益："];
+	}else {
+		[iconImage setImage:[UIImage imageNamed:@"金蚂蚁.png"]];
+		priceLabel.text = [NSString stringWithFormat:@"%d", tempPrice];
+		describeString = [describeString stringByAppendingString:@"公\n   价格："];
+		describeString = [describeString stringByAppendingString:priceLabel.text];
+		describeString = [describeString stringByAppendingString:@"个蚂蚁币"];
+		describeString = [describeString stringByAppendingString:@"\n总收益："];
+	}
+	
+	nameLabel.text = originAnimal.scientificNameCN;
+	
+	describeString = [describeString stringByAppendingString:[NSString stringWithFormat:@"%d", income]];
+	describeString = [describeString stringByAppendingString:@"个金蛋"];
+	describeLabel.numberOfLines = 3;
+	describeLabel.text = describeString;
+	
 }
 
 
 -(void) resultCallback:(NSObject *)value
 {
-/*	NSDictionary *itemDic;
-	NSArray *itemArray;
+	
 	switch (m_ntabFlag) {
-		case SHOP_POPVIEW:{
+		case BUY_ANIMAL:{
 			
-			itemDic = (NSDictionary *)[DataEnvironment sharedDataEnvironment].originalAnimals;
-			itemArray = [itemDic allKeys];
+			NSDictionary* dic = (NSDictionary*)value;
+			NSInteger code = [[dic objectForKey:@"code"] intValue];
+			switch (code) {
+				case 0:
+					[[FeedbackDialog sharedFeedbackDialog] addMessage:@"无道具信息!"];
+					break;
+				case 1:
+				{
+					[[FeedbackDialog sharedFeedbackDialog] addMessage:@"恭喜你购买物品成功!"];
+					
+					if (tempPrice > 0) 
+						((DataModelFarmerInfo *)[DataEnvironment sharedDataEnvironment].playerFarmerInfo).goldenEgg -= tempPrice * tempCount;
+					else
+						((DataModelFarmerInfo *)[DataEnvironment sharedDataEnvironment].playerFarmerInfo).antsCurrency -= tempPrice * tempCount;
+	
+					[[GameMainScene sharedGameMainScene] updateUserInfo];
+				}
+					break;
+				case 2:
+					[[FeedbackDialog sharedFeedbackDialog] addMessage:@"余额不足!"];
+					break;
+				case 3:
+					[[FeedbackDialog sharedFeedbackDialog] addMessage:@"操作正在进行，不能短时间连续购买!"];
+					break;
+				case 4:
+					[[FeedbackDialog sharedFeedbackDialog] addMessage:@"不能再买了!"];
+					break;
+				default:
+					[[FeedbackDialog sharedFeedbackDialog] addMessage:@"操作失败"];
+					break;
+			}
 		}
 			break;
+			
+		case SALE_ANIMAL:{
+				
+		}
+				break;
+			
 		default:
 			break;
 	}
-*/
 	
-//	ItemInfoPane *itemInfo = (ItemInfoPane *)button.target; 
-//	int tempPrice  = itemInfo.itemPrice;
-	
-	NSDictionary* dic = (NSDictionary*)value;
- 	NSInteger code = [[dic objectForKey:@"code"] intValue];
-	switch (code) {
-		case 0:
-			[[FeedbackDialog sharedFeedbackDialog] addMessage:@"无道具信息!"];
-			break;
-		case 1:
-		{
-			[[FeedbackDialog sharedFeedbackDialog] addMessage:@"恭喜你购买物品成功!"];
-//			if(curr_itemType == @"ant")
-//			{
-//				((DataModelFarmerInfo *)[DataEnvironment sharedDataEnvironment].playerFarmerInfo).antsCurrency -= tempPrice * tempCount;
-//			}
-//			else {
-//				((DataModelFarmerInfo *)[DataEnvironment sharedDataEnvironment].playerFarmerInfo).goldenEgg -= tempPrice * tempCount;
-//			}
-			[[GameMainScene sharedGameMainScene] updateUserInfo];
-		}
-			break;
-		case 2:
-			[[FeedbackDialog sharedFeedbackDialog] addMessage:@"余额不足!"];
-			break;
-		case 3:
-			[[FeedbackDialog sharedFeedbackDialog] addMessage:@"操作正在进行，不能短时间连续购买!"];
-			break;
-		case 4:
-			[[FeedbackDialog sharedFeedbackDialog] addMessage:@"不能再买了!"];
-			break;
-		default:
-			[[FeedbackDialog sharedFeedbackDialog] addMessage:@"操作失败"];
-			break;
-	}
 }
 
 -(void) faultCallback:(NSObject *)value
